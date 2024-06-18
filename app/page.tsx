48c36fbc4dbad29e67c93793dbf01e98f025a889
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2024-06-18 08:58:22 -0400

    refined landing page css a little

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,36 @@
-"use client";
-import "@/styles/Landing.css";
-import "@/app/globals.css";
-import { useActionState } from "react";
-import Image from "next/image";
-import Logo from '@/public/Logo.png';
-import { submitForm } from '@/app/actions';
-import { useFormState } from "react-dom";
+'use client'
+import '@/styles/Landing.css'
+import '@/app/globals.css'
+import { useActionState } from 'react'
+import Image from 'next/image'
+import Logo from '@/public/Logo.png'
+import { submitForm } from '@/app/actions'
+import { useFormState } from 'react-dom'
 
 export default function Home() {
-    const [state, formAction] = useFormState(submitForm, { email: "", message: "", isSubmitting: false });
+	const [state, formAction] = useFormState(submitForm, { email: '', message: '', isSubmitting: false })
 
-    return (
-        <div className='landing-page'>
-            <Image src={Logo} alt="Logo" width={120} height={120} />
-            <h3>Under Construction👷🏽</h3>
-
-            {state.message ? <p>{state.message}</p> : <p>If you'd like to be notified when we're ready, please leave us your email!</p>}
-
-            {!state.message && (
-                <form action={formAction}>
-                    <div className="InputTextBox flex flex-col">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            required
-                        />
-                    </div>
-                    <button type="submit" disabled={state.isSubmitting}>Notify Me</button>
-                </form>
-            )}
-        </div>
-    );
+	return (
+		<div className='landing-page'>
+			<Image src={Logo} alt='Logo' />
+			<div className='form-container'>
+				<h3>Under Construction👷🏽</h3>
+				{state.message ? (
+					<p>{state.message}</p>
+				) : (
+					<p>If you&apos;d like to be notified when we&apos;re ready, please leave us your email!</p>
+				)}
+				{!state.message && (
+					<form action={formAction}>
+						<div className='InputTextBox flex flex-col'>
+							<input type='email' placeholder='Email' name='email' required />
+						</div>
+						<button type='submit' disabled={state.isSubmitting}>
+							Notify Me
+						</button>
+					</form>
+				)}
+			</div>
+		</div>
+	)
 }
